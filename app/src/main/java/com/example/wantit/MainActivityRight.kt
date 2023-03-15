c5157bc1f1a8fe58af890a75e44a245f71da079a
givemeapassword <vt070502@yandex.ru>
2023-03-15 21:17:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/wantit/MainActivityRight.kt
@@ -0,0 +1,11 @@
+package com.example.wantit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityRight : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_right)
+    }
+}
