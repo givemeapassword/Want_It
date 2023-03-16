0d85b4facacff4a8a7e93adefe949716eaf0ecef
givemeapassword <vt070502@yandex.ru>
2023-03-16 16:28:33 +0300

    Реализация слайдов и примерно накинуты кнопки

--- app/src/main/java/com/example/wantit/MainActivity.kt
+++ app/src/main/java/com/example/wantit/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.wantit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+
+    val fragments: ArrayList<Fragment> = arrayListOf(
+        Page1Fragment(),
+        Page2Fragment(),
+        Page3Fragment()
+    )
+    val adapter = ViewPagerAdapter(fragments, this)
+    viewPager.adapter = adapter
     }
 }
