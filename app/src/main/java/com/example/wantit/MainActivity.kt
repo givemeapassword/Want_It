582279fa982446252efb4e3b6fac67bae671feee
givemeapassword <vt070502@yandex.ru>
2023-03-29 20:38:24 +0300

    Добавление: экрана загрузки,переход кнопки "настройка", решение проблемы со слайдом основных экранов

--- app/src/main/java/com/example/wantit/MainActivity.kt
+++ app/src/main/java/com/example/wantit/MainActivity.kt
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import android.content.Intent
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
 
-    val fragments: ArrayList<Fragment> = arrayListOf(
-        Page2Fragment(),
-        Page3Fragment()
-    )
-    val adapter = ViewPagerAdapter(fragments, this)
-    viewPager.adapter = adapter
+        val fragments: ArrayList<Fragment> = arrayListOf(
+            Page2Fragment(),
+            Page3Fragment()
+        )
+        val adapter = ViewPagerAdapter(fragments, this)
+        viewPager.adapter = adapter
+        }
+    fun ClickSetting(view: View){
+        val intent = Intent(this, SettingActivity::class.java)
+        startActivity(intent)
     }
 }
